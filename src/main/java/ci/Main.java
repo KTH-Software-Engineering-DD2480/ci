3c0e4b0f59c0c362c3803a86788e1e998ddbfdde
Christofer Nolander <christofer.nolander@gmail.com>
2022-02-07 13:52:20 +0100

    feat: add code skeleton with gradle build system
    fixes #2

---
+++ src/main/java/ci/Main.java
@@ -0,0 +1,8 @@
+package ci;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello CI!");
+    }
+}
+
---
+++ src/test/java/ci/MainTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainTest {
+    @Test
+    void alwaysTrue() {
+        assertEquals(3, 2 + 1);
+    }
+}
+
