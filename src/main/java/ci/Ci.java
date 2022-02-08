3b7d550af988c3d35cdc8e5107160121a761d72e
Kubha99 <1kunalbhatnagar@gmail.com>
2022-02-08 20:51:33 +0100

    Feat: added functionality to execute tests in specific directory

--- src/main/java/ci/Ci.java
+++ src/main/java/ci/Ci.java
@@ -1,15 +1,22 @@
+package ci;
+
 import java.io.*;
 import java.lang.*;
 
 public class Ci {
 
-    public static void Ci(){
+    public static void runTests(String pathToDirectory){
         try {
-            Runtime rt = Runtime.getRuntime();
-            Process process = rt.exec("./gradlew test");
+            // Create command to run in pathToDirectory
+            ProcessBuilder pb = new ProcessBuilder().command(new String[]{"./gradlew", "test"});
+            pb.directory(new File(pathToDirectory));
+            Process process = pb.start();
+
+            // Read output
             BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
             BufferedReader stdError = new BufferedReader(new InputStreamReader(process.getErrorStream()));
 
+            // Print output to console
             String error_string = null;
             while ((error_string = stdError.readLine()) != null) {
                 System.out.println(error_string);
