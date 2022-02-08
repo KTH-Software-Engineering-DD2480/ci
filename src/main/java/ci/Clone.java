3fcef7cbf9e4f27ae0eac6435ba3b6c2a7e5ee3b
jiayig <jiayig@kth.se>
2022-02-08 14:15:35 +0100

    feat: implement git clone
    fixes #17

--- src/main/java/ci/Clone.java
+++ src/main/java/ci/Clone.java
@@ -2,19 +2,21 @@ package ci;
 
 import java.io.IOException;
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import java.io.File;
 
 public class Clone {
     
-    public static boolean gitClone(String repo_url, String local_dir){
-        try{
+    // this function clones the repo and then checkout a specific commit
+    public static boolean gitClone(String repo_url, String local_dir, String commit_id) throws GitAPIException{
+
             // Clone the repository into the local_directory
-            Git git = Git.cloneRepository().setURI(repo_url).setDirectory(local_dir).call();
+            Git git = Git.cloneRepository().setURI(repo_url).setNoCheckout(true).setDirectory(new File(local_dir)).call();
+            // checkout commit with hash commit_id, detached HEAD state
+            git.checkout().setName(commit_id).call();
+
             return true;
-        } catch (Exception e){
-            return false;
-        }
-        
     }
+
 }
 
