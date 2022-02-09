8addcb45ee1a4ecfb5f495b096ec6e89a551a5f3
Philip Salqvist <philipsalqvist@MacBook-Pro-som-tillhor-Philip.local>
2022-02-09 13:55:07 +0100

    fix: 1. Propagate exceptions instead of printing error 2. Add url as parameter instead of hardcoding the url as constant 3. Use environment variable for access token instead of hardcoded constant
    Fixes: #11

--- src/main/java/ci/UpdateStatus.java
+++ src/main/java/ci/UpdateStatus.java
@@ -12,81 +12,61 @@ import java.net.http.HttpResponse.BodyHandlers;
 import org.json.JSONObject;
 
 public class UpdateStatus {
+    public enum Status {error, failure, pending, success}
 
     /**
-     * Takes strings `SHA` (unique id of commit) and `status` as inputs, and updates commit for the
-     * given `SHA`. 
+     * Takes strings `url` and `status` as inputs, and updates commit for the
+     * given SHA (unique id of commit) in `url`. 
      * Input `status` should be either "error", "failure", "pending" or "success".
-     * @param SHA
+     * @param url - - https://api.github.com/repos/{owner}/{repo}/statuses/{sha}
      * @param status
      */
-    public static void updateStatus(String SHA, String status) {
+    public static void updateStatus(String url, Status status) 
+        throws IOException, URISyntaxException, InterruptedException {
+        
+        String accessToken = getGitHubAccessTokenFromEnvironment();
+        HttpRequest request = HttpRequest.newBuilder()
+        .uri(new URI(url))
+        .headers("accept", "application/vnd.github.v3+json", "Authorization", accessToken)
+        .POST(HttpRequest.BodyPublishers.ofString("{\"state\":\"" + status + "\"}"))
+        .build();
+        
+        HttpClient.newBuilder()
+        .build()
+        .send(request, BodyHandlers.ofString());
 
-        if(!isValidStatus(status)){
-            System.out.println("Invalid input for status");
-            return;
-        }
-
-        String accessToken = "token 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";
-
-        try {
-            HttpRequest request = HttpRequest.newBuilder()
-            .uri(new URI("https://api.github.com/repos/KTH-Software-Engineering-DD2480/commit-status-test/statuses/" + SHA))
-            .headers("accept", "application/vnd.github.v3+json", "Authorization", accessToken)
-            .POST(HttpRequest.BodyPublishers.ofString("{\"state\":\"" + status + "\"}"))
-            .build();
-          
-            HttpResponse<String> response = HttpClient.newBuilder()
-            .build()
-            .send(request, BodyHandlers.ofString());
-
-            JSONObject json = new JSONObject(response.body());
-            System.out.println("json object: " + json);
-
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 
-    // Check that `status` is one of "error", "success", "pending" or "failure"
-    private static boolean isValidStatus(String status) {
-        if(status == "error" || status == "failure" || status == "success" || status == "pending") {
-            return true;
+    // load the GitHub secret token from an environemnt variable
+    private static String getGitHubAccessTokenFromEnvironment() {
+        String accessToken = System.getenv("GITHUB_ACCESS_TOKEN");
+        if (accessToken == null) {
+            throw new RuntimeException("GITHUB_ACCESS_TOKEN environment variable not set (must be set to the same value as the one used on GitHub)");
         }
-        return false;
+        return accessToken;
     }
 
     /**
-     * Takes a string `SHA` (unique id of commit) as input and returns `status` of the commit as a string.
+     * Takes a string `url` as input and returns `status` of the commit as a string.
      * Used as a helper method to test `updateStatus`.
-     * @param SHA
+     * @param url - https://api.github.com/repos/{owner}/{repo}/commits/{ref}/statuses
      * @return status
      */
-    public static String getStatus(String SHA) {
-        String status = "";
-        try {
-            HttpRequest request = HttpRequest.newBuilder()
-            .uri(new URI("https://api.github.com/repos/KTH-Software-Engineering-DD2480/commit-status-test/commits/" + SHA + "/status"))
-            .GET()
-            .build();
-            HttpResponse<String> response = HttpClient.newBuilder()
-            .build()
-            .send(request, BodyHandlers.ofString());
-            JSONObject json = new JSONObject(response.body());
-            status = json.getString("state").toString();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        
-        return status;
+    public static String getStatus(String url) 
+        throws IOException, URISyntaxException, InterruptedException {
+
+        HttpRequest request = HttpRequest.newBuilder()
+        .uri(new URI(url))
+        .GET()
+        .build();
+
+        HttpResponse<String> response = HttpClient.newBuilder()
+        .build()
+        .send(request, BodyHandlers.ofString());
+        JSONObject json = new JSONObject(response.body());
+
+        return json.getString("state").toString();
+
     }
 }
 
