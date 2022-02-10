9eb7c6544db53ebe77e04ecefd87cd9f94bba101
Christofer Nolander <christofer.nolander@gmail.com>
2022-02-10 19:18:39 +0100

    fix: broken test due to refactor and start job-queue
    resolves #35

--- src/main/java/ci/github/PushEventHandler.java
+++ src/main/java/ci/github/PushEventHandler.java
@@ -17,23 +17,34 @@ public class PushEventHandler implements EventHandler {
      */
     @Override
     public void handle(JSONObject body) throws IOException, ServletException {
+        WorkItem item = extractWorkItem(body);
+        try {
+            ci.JobQueueConsumer.addWork(item);
+        } catch (InterruptedException e) {
+            System.err.println("failed to add work item: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Extract a work item from the push event JSON body
+     * @param body the json body of a push event
+     * @return the corresponding work item
+     */
+    static WorkItem extractWorkItem(JSONObject body) {
         JSONObject repository = body.getJSONObject("repository");
         JSONObject headCommit = body.getJSONObject("head_commit");
 
         String head = body.getString("after");
 
         WorkItem item = new WorkItem();
+
         // Example: https://api.github.com/repos/nolanderc/ci-test-repo/statuses/{sha}
         item.statusUrl = repository.getString("statuses_url").replace("{sha}", head);
         item.repoUrl = repository.getString("url");
         item.ref = body.getString("ref");
         item.head = head;
         item.message = headCommit.getString("message");
-        
-        try {
-            ci.JobQueueConsumer.addWork(item);
-        } catch (InterruptedException e) {
-            System.err.println("failed to add work item: " + e.getMessage());
-        }
+
+        return item;
     }
 }
